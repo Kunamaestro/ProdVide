baf30b909be196f6b68f7609f64bf2572131a7a5
henri <henri-pham@live.fr>
2024-09-21 21:45:27 +0200

    M3 palette commence a etre fonctionnel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./Components/navbar/navbar.component";
+import { NavbarComponent } from "./Components/static/navbar/navbar.component";
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { FooterComponent } from "./Components/static/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    FooterComponent,
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Prodvide';
+  
 }
